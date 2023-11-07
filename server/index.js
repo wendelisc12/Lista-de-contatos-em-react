942ebc15d46a32589e6a399df3004ee34b898bc5
Wendel <wendel.isaac12@gmail.com>
2023-11-07 17:02:03 -0300

    feat: configurando rotas que vao ser usadas

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
-const express = require("express")
+import express from "express";
+import userRoutes from "./routes/usersRoute.js"
+import cors from "cors"
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+app.use("/", userRoutes)
 app.listen(8800)
